b68355d527acea2cc2fe206b10ea20fd8b25ed77
6uzmen <greolon1@gmail.com>
2022-01-15 15:47:33 -0300

    add dependencies and update config

--- dist/server.js
+++ dist/server.js
@@ -4,13 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
 const body_parser_1 = __importDefault(require("body-parser"));
-const app = (0, express_1.default)();
 const routes_1 = __importDefault(require("./routes"));
+dotenv_1.default.config();
+const serverPort = process.env.SERVER_PORT || 3000;
+const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use('/api', routes_1.default);
 // tslint:disable-next-line:no-console
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(serverPort, () => console.log(`Listening on port ${serverPort}`));
 exports.default = app;
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-const app = express();
 import routes from './routes';
+
+dotenv.config();
+
+const serverPort = process.env.SERVER_PORT || 3000;
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,6 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', routes);
 
 // tslint:disable-next-line:no-console
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(serverPort, () => console.log(`Listening on port ${serverPort}`));
 
 export default app;
