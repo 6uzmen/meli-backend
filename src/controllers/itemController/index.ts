32daa96ee65fb451c5f729b14939f717abfc21f0
6uzmen <greolon1@gmail.com>
2022-01-17 00:28:01 -0300

    prettier

--- src/controllers/itemController/index.ts
+++ src/controllers/itemController/index.ts
@@ -1,17 +1,20 @@
 import { itemService } from '../../services';
+import express from 'express';
 
-export const getItems = async (req: { searchParameter: string }) => {
+export const getItems = async (req: express.Request, res: express.Response) => {
   try {
-    const itemsArray = await itemService.getItemsBySearchParameter(req.searchParameter);
+    const itemsArray = await itemService.getItemsBySearchParameter(req.query.q.toString());
+    res.send('Ready!');
     console.log(itemsArray);
   } catch (e) {
     console.log(e.message);
   }
 };
 
-export const getItemDetails = async (req: { itemId: string }) => {
+export const getItemDetails = async (req: express.Request, res: express.Response) => {
   try {
-    const itemDetails = await itemService.getItemAndDescriptionById(req.itemId);
+    const itemDetails = await itemService.getItemAndDescriptionById(req.params.itemId);
+    res.send('Ready!');
     console.log(itemDetails);
   } catch (e) {
     console.log(e.message);
